971dc1e7ac79c22763405cda4a83532b329b9adb
Diogo-gallina <diogogallina20@gmail.com>
2024-10-25 07:48:43 -0300

    first commit

---
+++ src/main/java/br/com/JavaPlusoftMvc/JavaPlusoftMvcApplication.java
@@ -0,0 +1,13 @@
+package br.com.JavaPlusoftMvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPlusoftMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPlusoftMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/JavaPlusoftMvc/JavaPlusoftMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.JavaPlusoftMvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPlusoftMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
